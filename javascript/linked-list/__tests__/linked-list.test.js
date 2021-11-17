e361b96ec20ba97131670884ac5d5c9daa3ef7ed
Heather Bisgaard <vbchomp@gmail.com>
2021-11-17 13:31:06 -0800

    code challenge 6 resubmit

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,29 +1,44 @@
-"use strict";
+'use strict';
 
-//const Node = require('../lib/node.js');
 // Require our linked list implementation
-const LinkedList = require("../lib/ll.js");
-console.log("Linked;", LinkedList);
+const LinkedList = require('../../linked-list-insertion/lib/ll.js');
 
-// describe('Linked List', () => {
-//   it('works', () => {
-//     expect(true).toBeTruthy();
-//   });
-// });
-
-describe("Linked List", () => {
-  it("instantiate a new linked list", () => {
+describe('Linked List', () => {
+  it('instantiate a new linked list', () => {
     let list = new LinkedList();
     expect(list.head).toBeNull();
   });
 
-  // it(' to the head', () => {
-  //   let list.append('anyString');
-  //   expect(list.head.value).toEqual('anyString');
-  // });
+  it('should append successfully', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.append(3);
+    list.append(7);
+    // console.log('list.head:', list.head);
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(7);
+  });
 
-  // it('', () => {
-  //   let list.append(1);
-  //   expect(list.head.next.value).toEqual(1);
-  // });
+  it('should insert before successfully', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.insertBefore(3, 0);
+    list.insertBefore(4, 1);
+    expect(list.head.value).toEqual(3);
+    expect(list.head.next.value).toEqual(4);
+    expect(list.head.next.next.value).toEqual(1);
+  });
+
+  it('should insert after successfully', () => {
+    let list = new LinkedList();
+    list.append(1);
+    list.insertAfter(3, 0);
+    list.insertAfter(4, 1);
+    console.log('list.head:', list.head.next);
+    expect(list.head.value).toEqual(1);
+    expect(list.head.next.value).toEqual(3);
+    expect(list.head.next.next.value).toEqual(4);
+  });
+
 });
